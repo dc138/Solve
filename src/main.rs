28313c3b8c34e1ac4224848d7a3dec62202de0c1
DarthChungo <antonio@adaro.eu>
2022-06-13 14:47:08 +0200

    Added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 use phf::phf_map;
 use std::env;
+use std::fmt;
+
+#[derive(Debug)]
+struct SyntaxError {
+    expression: String,
+    full_expression: String,
+    message: String,
+    position: usize,
+}
+
+impl SyntaxError {
+    fn new(expr: String, full_expr: String, msg: String, pos: usize) -> SyntaxError {
+        SyntaxError {
+            expression: expr,
+            full_expression: full_expr,
+            message: msg,
+            position: pos,
+        }
+    }
+}
+
+impl fmt::Display for SyntaxError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "error while parsing token \"{}\" in expression \"{}\": {}, {} <-- HERE",
+            self.expression,
+            self.full_expression,
+            self.message,
+            &self.full_expression[..self.position]
+        )
+    }
+}
 
 static OPERATORS: phf::Map<char, usize> = phf_map! {
     '+' => 3,
@@ -9,14 +42,25 @@ static OPERATORS: phf::Map<char, usize> = phf_map! {
     '^' => 1,
 };
 
-fn evaluate(expr: &str) -> f64 {
+fn evaluate(expr: &str, full_expr: &str, offset: usize) -> Result<f64, SyntaxError> {
+    print!("parse {} with ctx {} at {}: ", expr, full_expr, offset);
+
+    if expr.chars().filter(|c: &char| *c == ' ').count() == expr.len() {
+        return Err(SyntaxError::new(
+            expr.to_owned(),
+            full_expr.to_owned(),
+            "trying to parse an empty token".to_owned(),
+            offset,
+        ));
+    }
+
     let first_char = expr.chars().nth(0).unwrap();
     let last_char = expr.chars().last().unwrap();
 
     if first_char == '"' && last_char == '"' {
         let inner = &expr[1..expr.len() - 1];
-        println!("parse {}: unwrap into {}", expr, inner);
-        return evaluate(inner);
+        println!("unwrap into {}", inner);
+        return evaluate(inner, full_expr, offset + 1);
     } else if first_char == '(' {
         let mut i: usize = 0;
         let mut par_level: isize = 1;
@@ -37,8 +81,8 @@ fn evaluate(expr: &str) -> f64 {
 
         if i == expr.len() - 1 {
             let inner = &expr[1..expr.len() - 1];
-            println!("parse {}: unwrap into {}", expr, inner);
-            return evaluate(inner);
+            println!("unwrap into {}", inner);
+            return evaluate(inner, full_expr, offset + 1);
         }
     }
 
@@ -82,33 +126,52 @@ fn evaluate(expr: &str) -> f64 {
 
     if split_char == ' ' {
         if let Ok(val) = expr.parse::<f64>() {
-            println!("parse {}: float", expr);
-            return val;
+            println!("float");
+            return Ok(val);
         }
     } else {
         let left = &expr[..split_pos];
         let right = &expr[split_pos + 1..];
 
-        println!("parse {}: {} {} {}", expr, left, split_char, right);
+        println!("{} {} {}", left, split_char, right);
 
-        let left = if left.is_empty() && (split_char == '+' || split_char == '-') {
-            0.
+        let left = if left.is_empty() {
+            if split_char == '+' || split_char == '-' {
+                Ok(0.)
+            } else {
+                Err(SyntaxError::new(
+                    expr.to_owned(),
+                    full_expr.to_owned(),
+                    format!("expected token before operator {}", split_char),
+                    offset + expr.len(),
+                ))
+            }
         } else {
-            evaluate(left)
-        };
-        let right = evaluate(right);
+            evaluate(left, full_expr, offset)
+        }?;
+
+        let right = if right.is_empty() {
+            Err(SyntaxError::new(
+                expr.to_owned(),
+                full_expr.to_owned(),
+                format!("expected token after {} operator", split_char),
+                offset + expr.len(),
+            ))
+        } else {
+            evaluate(right, full_expr, offset + split_pos + 1)
+        }?;
 
         return match split_char {
-            '+' => left + right,
-            '-' => left - right,
-            '*' => left * right,
-            '/' => left / right,
-            '^' => f64::powf(left, right),
-            _ => 0.,
+            '+' => Ok(left + right),
+            '-' => Ok(left - right),
+            '*' => Ok(left * right),
+            '/' => Ok(left / right),
+            '^' => Ok(f64::powf(left, right)),
+            _ => panic!("reached unexpected code block"),
         };
     }
 
-    panic!("Reached end of function without branching");
+    panic!("reached unexpected code block");
 }
 
 fn main() {
@@ -117,9 +180,20 @@ fn main() {
         std::process::exit(1);
     }
 
-    let expr = env::args().skip(1).fold(String::new(), |partial, current| {
-        format!("{}{}", partial, current)
-    });
+    let expr = env::args()
+        .skip(1)
+        .fold(String::new(), |mut partial, current| {
+            partial.push_str(&current);
+            partial
+        });
 
-    println!("{}", evaluate(&expr));
+    match evaluate(&expr, &expr, 0) {
+        Ok(res) => {
+            println!("{}", res);
+        }
+        Err(err) => {
+            println!("{}", err);
+            std::process::exit(1);
+        }
+    }
 }
